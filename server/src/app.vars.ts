7ca77939eeee4958fe6500976d8586f45d6b586c
viniciusanchieta <--global>
2022-11-06 22:21:38 -0300

    feat: generate ERD, create count pools endpoint and add cors

--- server/src/app.vars.ts
+++ server/src/app.vars.ts
@@ -2,4 +2,5 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const APP_PORT = Number(process.env.APP_PORT)
+export const APP_PORT = Number(process.env.APP_PORT);
+export const HOST_PORT = String(process.env.HOST_PORT);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,27 @@
 import Fastify from 'fastify'
-import { APP_PORT } from './app.vars'
+import cors from '@fastify/cors'
+import { APP_PORT, HOST_PORT } from './app.vars'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query'],
+});
 
 async function bootstrap(){
   const fastify = Fastify({
     logger: true
   })
 
-  fastify.get('/pools/count', async () => {
-    return { count: 1 }
+  await fastify.register(cors,{
+    origin: true // TODO: change to specific origin
   })
 
-  await fastify.listen({ port: APP_PORT})
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count();
+    return { count };
+  })
+
+  await fastify.listen({ port: APP_PORT, host: HOST_PORT })
 }
 
 bootstrap()
